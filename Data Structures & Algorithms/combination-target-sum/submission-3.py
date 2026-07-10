7ff985f9116f79c0285b0eaaf707fafc663fb4d6
Yiqing <89442858+qingqingqinga@users.noreply.github.com>
2026-07-10 10:49:44 +0800

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []          # 存放所有结果组合
+        temp = []         # 当前路径
+
+        # 递归函数，参数含义：
+        # start: 当前可以选取的起始下标（允许重复选自己）
+        # target: 剩余需要凑的目标值
+        def dfs(start,target):
+            if target == 0:
+                res.append(temp.copy())
+                return
+            if target < 0:
+                return
+            
+            for i in range(start,len(nums)):
+                if target - nums[i] >= 0:
+                    temp.append(nums[i])
+                    dfs(start + 1,target - nums[i])
+                    temp.pop()
+        dfs(0,9)
+        return res
+
+      
