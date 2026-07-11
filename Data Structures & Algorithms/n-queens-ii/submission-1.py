d9c1b95f5480c6bfc8c4c4cd859a3926fdc61c49
Yiqing <89442858+qingqingqinga@users.noreply.github.com>
2026-07-11 15:30:05 +0800

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        self.count = 0 #UnboundLocalError: cannot access local variable 'count' where it is not associated with a value
+        col = set()
+        posDiag = set() #r + c
+        negDiag = set() # r - c
+        board = [["."] * n for _ in range(n)]
+
+        def dfs(r):
+            if r >= n:
+                self.count += 1
+                return
+            
+            for c in range(n):
+
+               
+                if c in col or (r + c) in posDiag or (r -c ) in negDiag:
+                    continue
+                
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                dfs(r + 1)
+                
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+        dfs(0)
+        return self.count
+
+        
+
+
+
+                
+        
+        
