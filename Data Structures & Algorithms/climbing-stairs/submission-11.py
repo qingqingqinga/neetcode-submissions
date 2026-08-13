75d950b7522744da1abee2fa9eb4f800298d1bf6
Yiqing <89442858+qingqingqinga@users.noreply.github.com>
2026-08-13 10:00:54 +0800

    Add: climbing-stairs - submission-11

---
+++ Data Structures & Algorithms/climbing-stairs/submission-11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one,two = 1, 1 
+        for i in range(n - 1): #n-1很重要 因为n是个数 n-2代表索引
+        #i= 0 算的是dp[2]
+            temp = one
+            one = one + two
+            two = temp
+        return one
+
+        #dp[i] = dp[i - 1] + dp[ i - 2]
+       
