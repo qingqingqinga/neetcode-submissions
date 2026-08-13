971f949e2ee124bae3a2225874074ebbd629eb1d
Yiqing <89442858+qingqingqinga@users.noreply.github.com>
2026-08-13 11:11:20 +0800

    Add: house-robber-ii - submission-7

---
+++ Data Structures & Algorithms/house-robber-ii/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        
+        if n == 2:
+            return max(nums[0],nums[1])
+        
+        def dfs(nums):
+            m = len(nums)
+            prev2 = nums[0]
+            prev1 = max(nums[0],nums[1])
+
+            for i in range(2,m): #第三间房子 
+                res = max(prev1, prev2 + nums[i])
+                prev2 = prev1
+                prev1 = res
+            return prev1
+        return max(dfs(nums[1:n]),dfs(nums[0:n -1]))
+        
