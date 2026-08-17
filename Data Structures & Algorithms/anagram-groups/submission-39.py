e607625b92bbe80e1646f1516c3fb551a9b18262
Yiqing <89442858+qingqingqinga@users.noreply.github.com>
2026-08-17 16:14:56 +0800

    Add: anagram-groups - submission-39

---
+++ Data Structures & Algorithms/anagram-groups/submission-39.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]: #计数法和排序法都有
+        res  = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                key = ord(c) - ord('a')
+                count[key] += 1
+            
+            res[tuple(count)].append(s)
+        
+        return (list(res.values()))
+
+        
+        res = {} # count:s
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                key = ord(c) - ord('a') 
+                count[key] += 1
+            if tuple(count) not in res:
+                res[tuple(count)] = []
+                res[tuple(count)].append(s)
+            else: 
+                res[tuple(count)].append(s)
+        return list(res.values())
+
+
+     
