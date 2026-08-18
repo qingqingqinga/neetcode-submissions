7e1a6a397e991b3b5f8ee3538be0352dd13ab839
Yiqing <89442858+qingqingqinga@users.noreply.github.com>
2026-08-18 19:08:36 +0800

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+       
+        min_pre = 0  # 空前缀
+        cur = 0
+        res = float('-inf')
+        for num in nums:
+            cur += num
+            res = max(res, cur - min_pre)  # 当前前缀和 - 历史最小前缀和
+            min_pre = min(min_pre, cur)    # 更新历史最小前缀和
+        return res
+
+        
+
